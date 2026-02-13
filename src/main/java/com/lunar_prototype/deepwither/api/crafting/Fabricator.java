3cd91963ebfddf00af3943dec42d05abf7b0baeb
lninyu <59942911+lninyu@users.noreply.github.com>
2026-02-13 13:39:48 +0900

    feat(api.crafting): レシピ処理のインターフェースを追加

---
+++ src/main/java/com/lunar_prototype/deepwither/api/crafting/Fabricator.java
@@ -0,0 +1,22 @@
+package com.lunar_prototype.deepwither.api.crafting;
+
+import org.jspecify.annotations.NonNull;
+
+import java.util.Optional;
+
+/**
+ * レシピ処理用の(過度に)抽象的なインターフェース
+ *
+ * @apiNote ドメイン固有DTOを使用すると幸せになります。
+ * <br> 少なくとも、将来見たときに意味不明な型パラメータを見て発狂することは回避できるかもしれない。
+ *
+ * @implNote 実装方法 (is-a, has-a, etc...) は指定しません。
+ * <br> 継承したインターフェースを作るなり、パラメータ用DTOをぶちこむなりして扱って。
+ * <br> 意味を無視した使い方も許容するされるけども、{@link java.util.function.Function} の方がおすすめですよ。
+ *
+ * @param <Recipe> 入力(材料, 要求, コンテキストなど; 要するになんでも！)
+ * @param <Result> {@code nya?}
+ */
+public interface Fabricator<Recipe, Result> {
+    FabricationResult<Result> fabricate(@NonNull Recipe recipe);
+}
